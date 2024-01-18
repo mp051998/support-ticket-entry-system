f78c19310b5162c367ca52407bbc0e48e49772d2
Monish <mp051998@gmail.com>
2024-01-19 01:59:39 +0530

    added tickets list and orientation responsive elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
 import './App.css';
 import 'rsuite/dist/rsuite.min.css';
 
-import { Nav, Sidenav } from 'rsuite';
+import { Nav, Navbar, Sidenav } from 'rsuite';
+import { createContext, useEffect, useState } from 'react';
 
 import Agents from './screens/agents';
-import Dashboard from './screens/tickets';
 import DashboardIcon from '@rsuite/icons/legacy/Dashboard';
 import GroupIcon from '@rsuite/icons/legacy/Group';
 import Tickets from './screens/tickets';
 import { ToastContainer } from 'react-toastify';
-import { useState } from 'react';
+
+export const UserContext = createContext({ orientation: 'landscape' });
+
 
 function App() {
 
+  function useOrientation() {
+    const [orientation, setOrientation] = useState(
+      window.innerHeight > window.innerWidth ? 'portrait' : 'landscape'
+    );
+  
+    useEffect(() => {
+      const handleResize = () => {
+        setOrientation(window.innerHeight > window.innerWidth ? 'portrait' : 'landscape');
+      };
+  
+      window.addEventListener('resize', handleResize);
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+  
+    return orientation;
+  }
+  
+  const appOrientation = useOrientation();
   const [expanded, setExpanded] = useState(true);
   const [activeKey, setActiveKey] = useState('1');
+  const [activeComponent, setActiveComponent] = useState(<Tickets/>);
 
   const onNavSelect = (eventKey: string) => {
-    console.log("Event key: ", eventKey);
     setActiveKey(eventKey);
     const component = navItems.find(item => item.eventKey === eventKey)?.component;
     if (component) {
@@ -25,36 +47,59 @@ function App() {
     }
   }
 
-  const [activeComponent, setActiveComponent] = useState(<Dashboard />);
-
-  // Add navbar items here with associated logo and labels here
   const navItems = [
     {
       eventKey: '1',
       icon: <DashboardIcon />,
       label: 'Tickets',
-      component: Tickets
+      component: <Tickets/>
     },
     {
       eventKey: '2',
       icon: <GroupIcon />,
       label: 'Agents',
-      component: Agents
+      component: <Agents/>
     }
   ]
 
-  return (
-    <div className="App">
-      <ToastContainer />
-      <div style={{ height: '100vh', display: 'flex' }}>
-        <Sidenav appearance='inverse' expanded={expanded} defaultOpenKeys={['3', '4']} style={{ height: '100%', maxWidth: '12vw' }}
-        >
-          <Sidenav.Header>
-            <div style={{ padding: 20 }}>
-              {/* HEADING */}
-            </div>
-          </Sidenav.Header>
-          <Sidenav.Body style={{ textAlign: 'left' }}>
+  const renderView = () => {
+    if (appOrientation === 'landscape') {
+      return (
+        <div style={{ height: '100vh', display: 'flex' }}>
+          <Sidenav 
+            appearance='inverse'
+            expanded={expanded}
+            style={{ 
+              height: '100%', maxWidth: '12vw' 
+            }}
+            activeKey={activeKey}
+          >
+            <Sidenav.Header>
+              <div style={{ padding: 20 }}>
+                {/* HEADING */}
+              </div>
+            </Sidenav.Header>
+            <Sidenav.Body style={{ textAlign: 'left' }}>
+              <Nav activeKey={activeKey} onSelect={onNavSelect}>
+                {navItems.map(item => (
+                  <Nav.Item key={item.eventKey} eventKey={item.eventKey} icon={item.icon}>
+                    {item.label}
+                  </Nav.Item>
+                ))}
+              </Nav>
+            </Sidenav.Body>
+            <Sidenav.Toggle onToggle={expanded => setExpanded(expanded)} />
+          </Sidenav>
+
+          <div style={{padding: 20}}>
+            {activeComponent}
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div style={{ position: 'sticky', bottom: 0 }}>
+          <Navbar appearance='inverse'>
             <Nav activeKey={activeKey} onSelect={onNavSelect}>
               {navItems.map(item => (
                 <Nav.Item key={item.eventKey} eventKey={item.eventKey} icon={item.icon}>
@@ -62,14 +107,21 @@ function App() {
                 </Nav.Item>
               ))}
             </Nav>
-          </Sidenav.Body>
-          <Sidenav.Toggle onToggle={expanded => setExpanded(expanded)} />
-        </Sidenav>
-        <div style={{padding:'20px'}}>
-          {activeComponent}
+          </Navbar>
+          <div style={{padding: 20}}>
+            {activeComponent}
+          </div>
         </div>
-        
-      </div>
+      );
+    }
+  }
+
+  return (
+    <div className="App">
+      <UserContext.Provider value={{ orientation: appOrientation }}>
+        <ToastContainer />
+        {renderView()}
+      </UserContext.Provider>
     </div>
   );
 }
--- src/screens/tickets.tsx
+++ src/screens/tickets.tsx
@@ -1,9 +1,294 @@
+import { FlexboxGrid, List } from 'rsuite';
+import { cloneElement, useContext, useEffect, useState } from 'react';
 
-const Tickets = () => {
+import CreativeIcon from '@rsuite/icons/Creative';
+import DangerIcon from '@rsuite/icons/Danger';
+import PlusRoundIcon from '@rsuite/icons/PlusRound';
+import RemindOutlineIcon from '@rsuite/icons/RemindOutline';
+import UserCircleIcon from '@rsuite/icons/legacy/UserCircleO';
+import { UserContext } from '../App';
+
+const icons = {
+  'bug': <DangerIcon />,
+  'enhancement': <CreativeIcon />,
+  'feature': <PlusRoundIcon />,
+  'issue': <RemindOutlineIcon />
+};
+
+// Using dummy data till backend is ready
+const data = [
+  {
+    id: 1,
+    topic: "Bug Report",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    dateCreated: new Date("2022-01-01"),
+    severity: "High",
+    type: "bug",
+    assignedTo: "support-agent-1",
+    status: "New",
+    resolvedOn: null
+  },
+  {
+    id: 2,
+    topic: "Feature Request",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    dateCreated: new Date("2022-01-02"),
+    severity: "Low",
+    type: "feature",
+    assignedTo: "support-agent-2",
+    status: "Assigned",
+    resolvedOn: null
+  },
+  {
+    id: 3,
+    topic: "Performance Issue",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    dateCreated: new Date("2022-01-03"),
+    severity: "Medium",
+    type: "issue",
+    assignedTo: "support-agent-3",
+    status: "Resolved",
+    resolvedOn: new Date("2022-01-05")
+  },
+  {
+    id: 4,
+    topic: "UI Enhancement",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    dateCreated: new Date("2022-01-06"),
+    severity: "Low",
+    type: "enhancement",
+    assignedTo: "support-agent-4",
+    status: "New",
+    resolvedOn: null
+  },
+  {
+    id: 5,
+    topic: "Data Import Error",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    dateCreated: new Date("2022-01-07"),
+    severity: "Medium",
+    type: "bug",
+    assignedTo: "support-agent-5",
+    status: "Assigned",
+    resolvedOn: null
+  },
+  {
+    id: 6,
+    topic: "Login Issue",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    dateCreated: new Date("2022-01-08"),
+    severity: "High",
+    type: "issue",
+    assignedTo: "support-agent-6",
+    status: "New",
+    resolvedOn: null
+  },
+  {
+    id: 7,
+    topic: "Feature Enhancement",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    dateCreated: new Date("2022-01-09"),
+    severity: "Medium",
+    type: "feature",
+    assignedTo: "support-agent-7",
+    status: "New",
+    resolvedOn: null
+  },
+  {
+    id: 8,
+    topic: "Performance Optimization",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    dateCreated: new Date("2022-01-10"),
+    severity: "High",
+    type: "issue",
+    assignedTo: "support-agent-8",
+    status: "New",
+    resolvedOn: null
+  },
+  {
+    id: 9,
+    topic: "Data Export Error",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    dateCreated: new Date("2022-01-11"),
+    severity: "Low",
+    type: "bug",
+    assignedTo: "support-agent-9",
+    status: "New",
+    resolvedOn: null
+  },
+  {
+    id: 10,
+    topic: "Feature Request",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    dateCreated: new Date("2022-01-12"),
+    severity: "Medium",
+    type: "feature",
+    assignedTo: "support-agent-10",
+    status: "New",
+    resolvedOn: null
+  },
+  {
+    topic: "Bug Report",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    dateCreated: new Date("2022-01-13"),
+    severity: "High",
+    type: "bug",
+    assignedTo: "support-agent-11",
+    status: "New",
+    resolvedOn: null
+  },
+  {
+    topic: "Performance Issue",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    dateCreated: new Date("2022-01-14"),
+    severity: "Medium",
+    type: "issue",
+    assignedTo: "support-agent-12",
+    status: "New",
+    resolvedOn: null
+  },
+  {
+    topic: "UI Enhancement",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    dateCreated: new Date("2022-01-15"),
+    severity: "Low",
+    type: "enhancement",
+    assignedTo: "support-agent-13",
+    status: "New",
+    resolvedOn: null
+  },
+  {
+    topic: "Bug Report",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    dateCreated: new Date("2022-01-16"),
+    severity: "High",
+    type: "bug",
+    assignedTo: "support-agent-14",
+    status: "New",
+    resolvedOn: null
+  },
+  {
+    topic: "Feature Request",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    dateCreated: new Date("2022-01-17"),
+    severity: "Low",
+    type: "feature",
+    assignedTo: "support-agent-15",
+    status: "New",
+    resolvedOn: null
+  },
+  {
+    topic: "Performance Issue",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    dateCreated: new Date("2022-01-18"),
+    severity: "Medium",
+    type: "issue",
+    assignedTo: "support-agent-16",
+    status: "New",
+    resolvedOn: null
+  },
+];
+
+
+
+const styleCenter = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '75px'
+};
+
+const slimText = {
+  fontSize: '0.666em',
+  color: '#97969B',
+  fontWeight: 'lighter',
+  paddingBottom: 5
+};
+
+const titleStyle = {
+  paddingBottom: 5,
+  whiteSpace: 'nowrap',
+  fontWeight: 1000
+};
+
+const dataStyle = {
+  fontSize: '1.2em',
+  fontWeight: 500
+};
+
+// TODO: These heights and widths are not properly responsive, fix that
+const landscapeStyle = {
+  height: '85vh',
+  width: '20vw',
+  overflow: 'auto'
+}
+
+const portraitStyle = {
+  height: '75vh',
+  width: '100%',
+  overflow: 'auto'
+}
+
+function Tickets() {
+
+  const appContext = useContext(UserContext);
+  console.log("App context is: ", appContext);
   
+  console.log("Orientation of tickets is: ", appContext.orientation);
+
+  const [style, setStyle] = useState(landscapeStyle);
+
+  // const style = appContext.orientation === 'landscape' ? landscapeStyle : portraitStyle;
+  useEffect(() => {
+    console.log("Orientation changed to: ", appContext.orientation);
+    if (appContext.orientation === 'landscape') {
+      setStyle(landscapeStyle);
+    } else {
+      setStyle(portraitStyle);
+    }
+  }, [appContext.orientation])
+
   return (
     <div>
-      <h2 style={{textAlign:'left'}}>Dashboard</h2>
+      <h2 style={{textAlign:'left'}}>Tickets</h2>
+
+      <br/>
+
+      <List style={style} hover>
+        {data.map((item, index) => (
+          <List.Item key={item['id']} index={index + 1}>
+            <FlexboxGrid>
+              <FlexboxGrid.Item colspan={4} style={styleCenter}>
+                {cloneElement(icons[item['type'] as keyof typeof icons], {
+                  style: {
+                    color: 'darkgrey',
+                    fontSize: '1.5em'
+                  }
+                })}
+              </FlexboxGrid.Item>
+              <FlexboxGrid.Item
+                colspan={16}
+                style={{
+                  ...styleCenter,
+                  flexDirection: 'column',
+                  alignItems: 'flex-start',
+                  overflow: 'hidden'
+                }}
+              >
+                <div style={titleStyle}>{item['topic']}</div>
+                <div style={slimText}>{item['type']}</div>
+                <div style={slimText}>
+                  <div>
+                    <UserCircleIcon />
+                    {' ' + item['assignedTo']}
+                  </div>
+                  <div>{item['dateCreated'].toDateString()}</div>
+                </div>
+              </FlexboxGrid.Item>
+            </FlexboxGrid>
+          </List.Item>
+        ))}
+      </List>
     </div>
     
   );
